5b0a703cc24443b4f6b4e7bc06806b235ed35708
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-12-14 11:14:53 +0100

    feat: add login, register and logut actions

--- frontend/actions/register.ts
+++ frontend/actions/register.ts
@@ -1,5 +1,33 @@
 "use server";
 
+import apiClient from "@/api/client";
 import { Register } from "@/types/auth";
+import { cookies } from "next/headers";
 
-export async function registerAction(values: Register) {}
+export async function registerAction(values: Register) {
+  const { data, error } = await apiClient.POST("/register", {
+    body: {
+      username: values.name,
+      email: values.email,
+      lastName: values.firstName,
+      firstName: values.lastName,
+      password: values.password,
+    },
+  });
+
+  if (error) {
+    const errorMessage = Array.isArray(error)
+      ? error.map((err) => `${err.description}`).join(", ")
+      : `${error}`;
+    return { error: `${errorMessage}` };
+  }
+
+  if (data?.token) {
+    (await cookies()).set("token", data.token, {
+      httpOnly: true,
+      path: "/",
+    });
+  }
+
+  return { success: "You are registred!" };
+}
