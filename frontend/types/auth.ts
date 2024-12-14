620a6148d6a1fe27a77820864d2520ca3354022f
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-12-14 11:16:11 +0100

    feat: add types for auth and action response

--- frontend/types/auth.ts
+++ frontend/types/auth.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  email: z.string().email({
-    message: "Email is required",
+  username: z.string().min(1, {
+    message: "Username is required",
   }),
   password: z.string().min(1, {
     message: "Password is required",
@@ -19,6 +19,12 @@ export const registerSchema = z
     name: z.string().min(1, {
       message: "Name is required",
     }),
+    firstName: z.string().min(1, {
+      message: "First name is required",
+    }),
+    lastName: z.string().min(1, {
+      message: "Last name is required",
+    }),
     password: z.string().min(8, {
       message: "Minimum 8 characters required",
     }),
@@ -30,3 +36,17 @@ export const registerSchema = z
   });
 
 export type Register = z.infer<typeof registerSchema>;
+
+export const newPasswordSchema = z
+  .object({
+    password: z.string().min(8, {
+      message: "Minimum 8 characters required",
+    }),
+    confirmPassword: z.string().min(1, "Password confirmation is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords do not match",
+  });
+
+export type NewPassword = z.infer<typeof newPasswordSchema>;
---
+++ frontend/types/server.ts
@@ -0,0 +1,1 @@
+export type ActionReponse = { sucessMessage: string; errorMessage: string };
