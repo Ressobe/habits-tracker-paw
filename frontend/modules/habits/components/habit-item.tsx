290d0691d288ee50824a314e02dc5a65a774547e
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-12-17 15:58:42 +0100

    feat: mock crud for habit

--- frontend/modules/habits/components/habit-item.tsx
+++ frontend/modules/habits/components/habit-item.tsx
@@ -7,18 +7,27 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import clsx from "clsx";
+import { HabitDropdownMenu } from "./habit-dropdown-menu";
+import { Badge } from "@/components/ui/badge";
 
 type HabitItemProps = {
   name: string;
+  categoryName: string;
   selected?: boolean;
 };
 
-export function HabitItem({ name, selected }: HabitItemProps) {
+export function HabitItem({ name, categoryName, selected }: HabitItemProps) {
   return (
     <Card className={clsx(selected && "bg-secondary")}>
-      <CardHeader>
-        <CardTitle>{name}</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+      <CardHeader className="flex flex-row items-center justify-between">
+        <div className="space-y-2">
+          <CardTitle>{name}</CardTitle>
+          <CardDescription className="flex gap-x-2">
+            <Badge variant="outline">{categoryName}</Badge>
+            <Badge variant="outline">HIGH</Badge>
+          </CardDescription>
+        </div>
+        <HabitDropdownMenu />
       </CardHeader>
       <CardContent>
         <Button variant="secondary"> Mark as completed</Button>
