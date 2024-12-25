61ef005b25a14579a0ed7596663220c4f34b2148
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-12-25 13:00:09 +0100

    feat(frontend): filter habits with search, category, priority and status

--- frontend/components/filter-dropdown-menu.tsx
+++ frontend/components/filter-dropdown-menu.tsx
@@ -1,7 +1,8 @@
+'use client';
+
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
@@ -15,8 +16,36 @@ import {
 } from "@/components/ui/select";
 import { Button } from "./ui/button";
 import { SlidersHorizontal } from "lucide-react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useCategoriesStore } from "@/stores/use-categories-store";
 
 export function FilterDropdownMenu() {
+  const categories = useCategoriesStore((state) => state.categories) ?? [];
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const category = searchParams.get("category") || "";
+  const priority = searchParams.get("priority") || "";
+  const status = searchParams.get("status") || "";
+
+  const updateQueryParam = (key: string, value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+
+    if (value) params.set(key, value);
+    else params.delete(key);
+
+    router.push(`?${params.toString()}`);
+  };
+
+  const resetFilters = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.delete("category");
+    params.delete("priority");
+    params.delete("status");
+
+    router.push(`?${params.toString()}`);
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -30,18 +59,27 @@ export function FilterDropdownMenu() {
         <div className="flex flex-col items-start p-2">
           <div className="flex w-full justify-between items-center py-1">
             <span className="text-sm text-muted-foreground">Category</span>
-            <Button size="sm" variant="ghost">
+            <Button
+              size="sm"
+              variant="ghost"
+              onClick={() => updateQueryParam("category", "")}
+            >
               Clear
             </Button>
           </div>
-          <Select>
+          <Select
+            value={category}
+            onValueChange={(value) => updateQueryParam("category", value)}
+          >
             <SelectTrigger>
-              <SelectValue />
+              <SelectValue placeholder="Select category" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="light">Category1</SelectItem>
-              <SelectItem value="dark">Category2</SelectItem>
-              <SelectItem value="system">Category3</SelectItem>
+              {categories.map((item) => (
+                <SelectItem key={item.id} value={`${item.id}`}>
+                  {item.name}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
         </div>
@@ -49,18 +87,25 @@ export function FilterDropdownMenu() {
         <div className="flex flex-col items-start p-2">
           <div className="flex w-full justify-between items-center py-1">
             <span className="text-sm text-muted-foreground">Priority</span>
-            <Button size="sm" variant="ghost">
+            <Button
+              size="sm"
+              variant="ghost"
+              onClick={() => updateQueryParam("priority", "")}
+            >
               Clear
             </Button>
           </div>
-          <Select>
+          <Select
+            value={priority}
+            onValueChange={(value) => updateQueryParam("priority", value)}
+          >
             <SelectTrigger>
-              <SelectValue />
+              <SelectValue placeholder="Select priority" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="light">LOW</SelectItem>
-              <SelectItem value="dark">MEDIUM</SelectItem>
-              <SelectItem value="system">HIGH</SelectItem>
+              <SelectItem value="1">LOW</SelectItem>
+              <SelectItem value="2">MEDIUM</SelectItem>
+              <SelectItem value="3">HIGH</SelectItem>
             </SelectContent>
           </Select>
         </div>
@@ -68,24 +113,32 @@ export function FilterDropdownMenu() {
         <div className="flex flex-col items-start p-2">
           <div className="flex w-full justify-between items-center py-1">
             <span className="text-sm text-muted-foreground">Status</span>
-            <Button size="sm" variant="ghost">
+            <Button
+              size="sm"
+              variant="ghost"
+              onClick={() => updateQueryParam("status", "")}
+            >
               Clear
             </Button>
           </div>
-          <Select>
+          <Select
+            value={status}
+            onValueChange={(value) => updateQueryParam("status", value)}
+          >
             <SelectTrigger>
-              <SelectValue />
+              <SelectValue placeholder="Select status" />
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="light">Completed</SelectItem>
-              <SelectItem value="dark">Not completed</SelectItem>
+              <SelectItem value="completed">Completed</SelectItem>
+              <SelectItem value="not_completed">Not completed</SelectItem>
             </SelectContent>
           </Select>
         </div>
         <DropdownMenuSeparator />
         <div className="flex justify-between p-2">
-          <Button variant="outline">Reset</Button>
-          <Button variant="secondary">Apply</Button>
+          <Button variant="outline" onClick={resetFilters}>
+            Reset
+          </Button>
         </div>
       </DropdownMenuContent>
     </DropdownMenu>
